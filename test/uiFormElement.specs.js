59fd93069587876386774e509dc182fcf30def87
mo valipour <valipour@gmail.com>
2015-04-04 19:49:13 +0100

    fix issue with switched fiels

--- test/uiFormElement.specs.js
+++ test/uiFormElement.specs.js
@@ -2,23 +2,22 @@ describe('directive: ui-form-element', function () {
   var element, scope;
   beforeEach(module('apogee.ui-forms'));
 
-  _.mockUniqueId(['some-id-1234']);
-
   var compile = function(html){
     inject(function($rootScope, $compile) {
       scope = $rootScope.$new();
       element = $compile(html)(scope);
-      element = $(element).find('[sut]');
-
       scope.$digest();
     });
   };
 
-  var html = '<form ui-form><div ui-form-element="somefield"><div sut ui-form-label></div></div></form>';
-  beforeEach(compile.bind(this, html));
+  it('throws when no field is specified', function () {
+    var html = '<form ui-form><div ui-form-element></div></form>';
+    expect(compile.bind(this, html)).to.throw('You must provide a field name for form-element');
+  });
 
-  it('sets correct for attribute', function () {
-    expect(element.attr('for')).to.equal('fe_some-id-1234');
+  it('throws when no field is specified in options', function () {
+    var html = '<form ui-form><div ui-form-element="{ something: 1234 }"></div></form>';
+    expect(compile.bind(this, html)).to.throw('You must provide a field name for form-element');
   });
 
 });
