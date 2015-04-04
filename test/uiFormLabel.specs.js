d9d5a3bb483719dc763362fda14ff0d4a9b91c8b
mo valipour <valipour@gmail.com>
2015-04-04 20:56:07 +0100

    more tests for ui-form-label

--- test/uiFormLabel.specs.js
+++ test/uiFormLabel.specs.js
@@ -14,11 +14,22 @@ describe('directive: ui-form-element', function () {
     });
   };
 
-  var html = '<form ui-form><div ui-form-element="somefield"><div sut ui-form-label></div></div></form>';
+  var html = '<form ui-form><div ui-form-element="somefield">' +
+    '<div sut ui-form-label ng-class="{ some: true }"><span ng-class="{ sub: true }"></span></div>' +
+    '</div></form>';
   beforeEach(compile.bind(this, html));
 
   it('sets correct for attribute', function () {
     expect(element.attr('for')).to.equal('fe_some-id-1234');
+  });
+
+  it('compiles other directives on same element', function () {
+    expect(element.hasClass('some')).to.equal(true);
+  });
+
+  it('compiles sub elements', function () {
+    var child = element.find('span');
+    expect(child.hasClass('sub')).to.equal(true);
   });
 
 });
